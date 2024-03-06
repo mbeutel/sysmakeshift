b0b4258128ca4ea1a3c42ddf2019266fdb5f4370
Moritz Beutel <moritz.beutel@ziti.uni-heidelberg.de>
2024-03-06 15:35:39 +0100

    Add basic test for `physical_core_ids()`

--- test/test-thread.cpp
+++ test/test-thread.cpp
@@ -1,9 +1,11 @@
 
-#include <thread> // for thread::hardware_concurrency()
+#include <thread>// for thread::hardware_concurrency()
 
 #include <sysmakeshift/thread.hpp>
 
 #include <iostream>
+
+#include <gsl-lite/gsl-lite.hpp>
 
 #include <catch2/catch_test_macros.hpp>
 
@@ -16,4 +18,27 @@ TEST_CASE("physical_concurrency() returns correct value")
 
     CHECK(physicalConcurrency != 0);
     CHECK(physicalConcurrency <= std::thread::hardware_concurrency());
+}
+
+TEST_CASE("physical_core_ids() returns correct number of thread ids")
+{
+    int physicalConcurrency = sysmakeshift::physical_concurrency();
+    auto physicalCoreIds = sysmakeshift::physical_core_ids();
+    std::cout << "Physical core ids: [";
+    bool first = true;
+    for (auto id : physicalCoreIds)
+    {
+        if (!first)
+        {
+            std::cout << ", ";
+        }
+        first = false;
+        std::cout << id;
+    }
+    std::cout << "]\n";
+
+    if (!physicalCoreIds.empty())
+    {
+        CHECK(gsl_lite::ssize(physicalCoreIds) == physicalConcurrency);
+    }
 }
